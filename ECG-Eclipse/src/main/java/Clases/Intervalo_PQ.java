5eab75bbd0c31d219498c838fb71a2d45630eaac
M.Amrou <ma.labiednasser@um.es>
2025-11-12 21:50:15 +0100

    Insertar reglas

--- ECG-Eclipse/src/main/java/Clases/Intervalo_PQ.java
+++ ECG-Eclipse/src/main/java/Clases/Intervalo_PQ.java
@@ -1,6 +1,6 @@
 package Clases;
 // CREO QUE NO HACE FALTA
-public class Intervalo_PQ extends Intervalo{
+public class Intervalo_PQ extends Intervalo {
 
 	public Intervalo_PQ(int inicio, int fin, int duracion) {
 		super(inicio, fin, duracion);
