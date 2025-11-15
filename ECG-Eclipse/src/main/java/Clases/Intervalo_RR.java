7c3663e9b3da0b6da5dbd63c539b706dba783071
M.Amrou <ma.labiednasser@um.es>
2025-11-15 19:39:48 +0100

    Falta solo la contraccion ventricular prematura

---
+++ ECG-Eclipse/src/main/java/Clases/Intervalo_RR.java
@@ -0,0 +1,10 @@
+package Clases;
+
+public class Intervalo_RR extends Intervalo {
+
+	public Intervalo_RR(int inicio, int fin, int duracion) {
+		super(inicio, fin, duracion);
+		// TODO Auto-generated constructor stub
+	}
+
+}
